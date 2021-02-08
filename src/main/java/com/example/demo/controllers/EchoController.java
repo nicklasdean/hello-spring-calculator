f7f24cefe15bd5ccc03d76edc048da1e17408f30
dean <nifr@kea.dk>
2021-02-08 11:26:21 +0100

    Comments

--- src/main/java/com/example/demo/controllers/EchoController.java
+++ src/main/java/com/example/demo/controllers/EchoController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class EchoController {
     //Remember to provide parameters to calculate
     //Otherwise a white-label error will occur
+
+    //A parameter is provided by localhost:8080?[name of RequestParam]=[Parameter]
+    //E.g: localhost:8080?stringFromParameter=HiWorld
+    //Should yield the result HiWorld
     @GetMapping("/echo")
     @ResponseBody
     public String echo(@RequestParam String stringFromParameter) {
